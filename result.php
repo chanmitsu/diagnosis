1be2c628f9cd3cae2f6c190b0de327b8bde64f4a
Mtuhiko Gusikenn <chanmitsu35@gmail.com>
2020-06-05 18:52:54 +0900

    HTML完成

---
+++ result.php
@@ -0,0 +1,30 @@
+<?php
+
+var_dump($_POST);
+
+
+$q1 = $_POST["Q1"];
+$q2 = $_POST["Q2"];
+$q3 = $_POST["Q3"];
+$q4 = $_POST["Q4"];
+$q5 = $_POST["Q5"];
+$q6 = $_POST["Q6"];
+$q7 = $_POST["Q7"];
+$q8 = $_POST["Q8"];
+$q9 = $_POST["Q9"];
+$q10 = $_POST["Q10"];
+
+echo $q1;
+echo $q2;
+echo $q3; 
+echo $q4; 
+echo $q5;
+echo $q6;
+echo $q7; 
+echo $q8; 
+echo $q9; 
+echo $q10;
+
+
+
+?>
